702efaeb5c0f8a624c3c5175b8354c242c2d0660
Kimi323 <wangmengya2010@yahoo.co.jp>
2020-12-28 15:21:55 +0900

    add css for common area and restaurant

--- common.js
+++ common.js
@@ -1,5 +1,15 @@
+// show home tab by default
+window.onload = function() {
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    if(i == 0) {
+      tabcontent[i].style.display = "block";
+    }
+  }
+}
+
+// show tabcontent when tab is clicked
 function openTab(e, tabName) {
-    // Declare all variables
     var i, tabcontent, tablinks;
   
     // Hide all elements with class="tabcontent" by default
